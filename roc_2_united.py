c7d1c5e9e58ae907ca8ab300d5d69fc82636c7c7
nikikapui <61835324+nikikapui@users.noreply.github.com>
2023-05-06 19:30:19 +0200

    Update roc_2_united.py

--- roc_2_united.py
+++ roc_2_united.py
@@ -79,6 +79,7 @@ print("united.csv")
 df_train = pd.read_csv("payloads/united/skip_gram/train.csv")
 X_train = df_train.loc[:, df_train.columns != 'Label']
 
+#Change name of test dataset here
 df_test = pd.read_csv("payloads/SQLi_2/plain/test.csv")
 X_test = df_test.loc[:, df_test.columns != 'Label']
 y_test = df_test['Label']
